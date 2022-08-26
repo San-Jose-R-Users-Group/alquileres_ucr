a0dda373eadeb1d088acfb6bcc42f84e5b1d3797
Carlos Agüero B <dev.aguero@gmail.com>
2022-08-26 10:46:11 -0600

    guarda el archivo en .csv y no en .qs

--- R/descargar_formulario_drive.R
+++ R/descargar_formulario_drive.R
@@ -1,11 +1,9 @@
 
 descargar_formulario <- function(name = NA_character_, path = "") {
-  checkmate::assert_path_for_output(path,extension = "qs")
+  checkmate::assert_path_for_output(path,extension = "csv")
   checkmate::assert_character(name)
   
   temp <- tempfile(fileext = ".csv")
-  googledrive::drive_download(name, type = "csv", path = temp)
-  df <- readr::read_csv(temp,show_col_types = FALSE)
-  qs::qsave(df, path)
+  googledrive::drive_download(name, type = "csv", path = path)
   return(path)
 }
