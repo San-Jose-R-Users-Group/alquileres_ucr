350a4d40fb4f4d46a09ecfc7758844bed4c589e0
Carlos Agüero B <dev.aguero@gmail.com>
2022-08-26 16:04:50 -0600

    Se silencia mensases de la función que descarga de google drive

--- R/descargar_formulario_drive.R
+++ R/descargar_formulario_drive.R
@@ -1,9 +1,11 @@
 
-descargar_formulario <- function(name = NA_character_, path = "") {
-  checkmate::assert_path_for_output(path,extension = "csv")
+descargar_form<- function(name = NA_character_, path = "") {
+  checkmate::assert_path_for_output(path, extension = "csv", overwrite = TRUE)
   checkmate::assert_character(name)
   
   temp <- tempfile(fileext = ".csv")
-  googledrive::drive_download(name, type = "csv", path = path)
+  suppressMessages(
+    googledrive::drive_download(name, type = "csv", path = path,overwrite = TRUE)
+  )
   return(path)
 }
